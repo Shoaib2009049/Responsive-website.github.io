7142317aabecb7e7e2fbfffb0571421e2ce5d17f
Shoaib Khan <83698945+Shoaib2009049@users.noreply.github.com>
2023-03-09 10:48:37 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,44 @@
+<!--<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">--
+coustom css-->
+<link href="css/style.css" rel="stylesheet" type="text/css"/>
+<!--fonts-->
+<link href='//fonts.googleapis.com/css?family=Open+Sans:300,300italic,400italic,400,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+
+            
+          <div class="copyright">
+    <!-- container -->
+    <div class="container">
+      <div class="copyright-left">
+      <p>© 2023 Responsive Stationary System </p>
+      </div>
+      <div class="copyright-right">
+        <ul>
+          <li><a href="#" class="twitter"> </a></li>
+          <li><a href="#" class="twitter facebook"> </a></li>
+          <li><a href="#" class="twitter chrome"> </a></li>
+          <li><a href="#" class="twitter pinterest"> </a></li>
+          <li><a href="#" class="twitter linkedin"> </a></li>
+          <li><a href="#" class="twitter dribbble"> </a></li>
+        </ul>
+      </div>
+      <div class="clearfix"> </div>
+      
+    </div>
+    <!-- //container -->
+    <!---->
+<script type="text/javascript">
+    $(document).ready(function() {
+        /*
+        var defaults = {
+        containerID: 'toTop', // fading element id
+        containerHoverID: 'toTopHover', // fading element hover id
+        scrollSpeed: 1200,
+        easingType: 'linear' 
+        };
+        */
+    $().UItoTop({ easingType: 'easeOutQuart' });
+});
+</script>
+<a href="#to-top" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+  </div><!--/copy-rights-->
---
+++ includes/forgot_password_modal.php
@@ -0,0 +1,51 @@
+<div class="modal fade" id="forgot_password-modal" tabindex="-1" role="dialog" aria-labelledby="forgot_password-heading" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="forgot_password-heading">Forgot Password</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="forgot_password-form" class="form" role="form" method="post" action="api/forgot_password_submit.php">
+                <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-envelope"></i>
+                            </span>
+                        </div>
+                        <input type="email" class="form-control" name="email" placeholder="Email" required>
+                    </div> 
+                    
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-lock"></i>
+                            </span>
+                        </div>
+                        <input type="password" class="form-control" name="new_password" placeholder="New_Password" minlength="6" required>
+                    </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-lock"></i>
+                            </span>
+                        </div>
+                        <input type="password" class="form-control" name="confirm_password" placeholder="Confirm_Password" minlength="6" required>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-block btn-primary">Confirm</button>
+                    </div>
+                </form>
+            </div>
+
+            <div class="modal-footer">
+                <span>Already have an account?
+                    <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#login-modal">Login</a>  
+                </span>
+                </div>
+        </div>
+    </div>
+</div>
